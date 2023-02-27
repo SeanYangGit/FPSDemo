79485ba0fcacc29cbbf54992ec9fc432bc87a530
yanghan4 <yanghan4@kingsoft.com>
2023-02-27 14:14:02 +0800

    feat: 项目初传

---
+++ Source/FPSDemo/FPSDemoGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FPSDemoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FPSDEMO_API AFPSDemoGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
